b0cc6a3364019d4a0f94373c19b8c07c1d919f97
wang0nya <kwangonya@gmail.com>
2018-06-06 20:39:54 +0300

    add firebase, angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBeqDR6Rs-OvM95qDDsmE-e6lMzOwru6nE',
+    authDomain: 'recipes-78f3c.firebaseapp.com',
+    databaseURL: 'https://recipes-78f3c.firebaseio.com',
+    projectId: 'recipes-78f3c',
+    storageBucket: 'recipes-78f3c.appspot.com',
+    messagingSenderId: '549231605480'
+  }
 };
